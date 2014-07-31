e2cb061b614106ae23738976948c6cca973a349d
Apricot-iOS <18675760522@163.com>
2014-07-31 11:22:30 +0800

    OC项目

---
+++ gitTest/gitTest/AppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  AppDelegate.h
+//  gitTest
+//
+//  Created by TalkWeb on 14-7-31.
+//  Copyright (c) 2014年 TalkWeb. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@end
---
+++ gitTest/gitTest/main.m
@@ -0,0 +1,18 @@
+//
+//  main.m
+//  gitTest
+//
+//  Created by TalkWeb on 14-7-31.
+//  Copyright (c) 2014年 TalkWeb. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+#import "AppDelegate.h"
+
+int main(int argc, char * argv[])
+{
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([AppDelegate class]));
+    }
+}
